af079b602de784be83c9fc22f2afa6868abdd85d
Kacper Sagnowski <ksagnowski@gmail.com>
2023-05-22 10:46:43 +0200

    Switch to LunarVim

--- home/.config/nvim_oldest/lua/user/plugins/lsp/init.lua
+++
@@ -1,3 +0,0 @@
-require("user.plugins.lsp.servers")
-require("user.plugins.lsp.null-ls")
-require("user.plugins.lsp.rust-tools")
--- home/.config/nvim_oldest/lua/user/plugins/mason.lua
+++
@@ -1,6 +0,0 @@
-local status_ok_mason, mason = pcall(require, "mason")
-if not status_ok_mason then
-  return
-end
-
-mason.setup()
