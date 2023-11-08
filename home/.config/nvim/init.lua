30e3f72bad547bf7a18daa609e9ea91d21cd7783
Kacper Sagnowski <ksagnowski@gmail.com>
2023-11-08 15:54:50 +0100

    Update basic nvim config

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -273,12 +273,14 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+vim.o.relativenumber = true
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+vim.keymap.set('i', 'jk', "<ESC>", { noremap = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
