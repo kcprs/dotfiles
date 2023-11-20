2771bc362034c7adca8736ab5c54d5a16e56c3a9
Kacper Sagnowski <ksagnowski@gmail.com>
2023-11-20 13:14:46 +0100

    Remove some comments

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -5,9 +5,6 @@ local keymaps = require "custom.keymaps"
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 keymaps.setup_leader()
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -20,13 +17,6 @@ if not vim.loop.fs_stat(lazypath) then
   }
 end
 vim.opt.rtp:prepend(lazypath)
-
--- [[ Configure plugins ]]
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
---
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
 require('lazy').setup("plugins")
 
 keymaps.setup_basic()
