07d75854d1422502dd6efdd1571b45c152d46459
Kacper Sagnowski <ksagnowski@gmail.com>
2023-12-01 11:57:56 +0100

    Add nvim surround

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -170,5 +170,12 @@ return {
         position = "right"
       }
     }
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    opts = {}
+  },
+
   }
 }
