a7d8746c82c284617345c1bf752fc617aa97c87c
Kacper Sagnowski <ksagnowski@gmail.com>
2024-02-16 22:59:28 +0100

    Set up toggleterm

--- home/.config/nvim/lua/custom/keymaps.lua
+++ home/.config/nvim/lua/custom/keymaps.lua
@@ -175,8 +175,36 @@ function M.set_neotree()
   vim.keymap.set("n", "<leader>eG", ":Neotree toggle action=focus source=git_status position=float<cr>", { desc = "Toggle Neotree float - [e]xplore [G]it" })
 end
 
--- function M.toggleterm_get_open_mapping()
---   return [[<c-`>]]
--- end
+function M.toggleterm_get_open_mapping()
+  return [[<c-\>]]
+end
+
+function M.toggleterm_set()
+  -- set up dedicated command to open a terminal with lazygit
+  local Terminal  = require('toggleterm.terminal').Terminal
+  local lazygit = Terminal:new({
+    cmd = "lazygit",
+    dir = "git_dir",
+    direction = "float",
+    float_opts = {
+      border = "curved",
+    },
+    -- function to run on opening the terminal
+    on_open = function(term)
+      vim.cmd("startinsert!")
+      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+    end,
+    -- function to run on closing the terminal
+    on_close = function()
+      vim.cmd("startinsert!")
+    end,
+  })
+
+  function LazyGitToggle()
+    lazygit:toggle()
+  end
+
+  vim.keymap.set("n", "<leader>G", "<cmd>lua LazyGitToggle()<CR>", {noremap = true, silent = true}, "[G]it")
+end
 
 return M
--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -231,13 +231,14 @@ return {
     end
   },
 
-  -- {
-  --   "akinsho/toggleterm.nvim",
-  --   config = function ()
-  --     require("toggleterm").setup{
-  --       open_mapping = require("custom.keymaps").toggleterm_get_open_mapping(),
-  --       terminal_mappings = true,
-  --     }
-  --   end
-  -- }
+  {
+    "akinsho/toggleterm.nvim",
+    config = function ()
+      require("toggleterm").setup{
+        open_mapping = require("custom.keymaps").toggleterm_get_open_mapping(),
+        terminal_mappings = true,
+      }
+    require("custom.keymaps").toggleterm_set()
+    end
+  }
 }
