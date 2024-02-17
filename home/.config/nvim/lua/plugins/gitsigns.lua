df30fd40ac2c17e111eba87b2e4decc725cd2c21
Kacper Sagnowski <ksagnowski@gmail.com>
2024-02-17 18:34:30 +0100

    Split plugin configs into separate files

---
+++ home/.config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,16 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    -- See `:help gitsigns.txt`
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+    on_attach = function(bufnr)
+      require("custom.keymaps").gitsigns_on_attach(bufnr)
+    end,
+  },
+}
