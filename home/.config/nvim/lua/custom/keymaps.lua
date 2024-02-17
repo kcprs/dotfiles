cd56594bb15eb231806d822a5b252fccc05e15fa
Kacper Sagnowski <ksagnowski@gmail.com>
2024-02-17 20:18:30 +0100

    Set up harpoon

--- home/.config/nvim/lua/custom/keymaps.lua
+++ home/.config/nvim/lua/custom/keymaps.lua
@@ -211,20 +211,4 @@ function M.toggleterm_set()
   vim.keymap.set("n", "<leader>G", "<cmd>lua LazyGitToggle()<CR>", {noremap = true, silent = true}, "[G]it")
 end
 
-function M.harpoon_set()
-  local harpoon = require("harpoon")
-
-  vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
-  vim.keymap.set("n", "<leader>hs", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-  -- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-  -- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-  -- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-  -- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-
-  -- Toggle previous & next buffers stored within Harpoon list
-  vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-  vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-end
-
 return M
