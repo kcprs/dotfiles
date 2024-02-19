56740dcd5335885f462a63dc006eb84f84c7b242
Kacper Sagnowski <ksagnowski@gmail.com>
2024-02-19 22:13:18 +0100

    Fix harpoon mappings

--- home/.config/nvim/lua/plugins/harpoon.lua
+++ home/.config/nvim/lua/plugins/harpoon.lua
@@ -10,8 +10,8 @@ return {
     vim.keymap.set("n", "<leader>ht", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "[h]arpoon [t]oggle" })
 
     vim.keymap.set("n", "<leader>hh", function() harpoon:list():select(1) end, { desc = "[h]arpoon file 1" })
-    vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(2) end, { desc = "[h]arpoon file 2" })
-    vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(3) end, { desc = "[h]arpoon file 3" })
+    vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(2) end, { desc = "[h]arpoon file 2" })
+    vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(3) end, { desc = "[h]arpoon file 3" })
     vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(4) end, { desc = "[h]arpoon file 4" })
 
     -- Toggle previous & next buffers stored within Harpoon list
