63002dcd01bbc175a63fec762e8a733334fd3450
Kacper Sagnowski <ksagnowski@gmail.com>
2024-03-17 18:08:58 +0100

    Change stylua config, apply formatting

--- home/.config/nvim/lua/plugins/gitsigns.lua
+++ home/.config/nvim/lua/plugins/gitsigns.lua
@@ -38,16 +38,19 @@ return {
                 return "<Ignore>"
             end, { expr = true, desc = "Jump to previous hunk" })
 
-
             map("n", "<leader>gp", gs.preview_hunk, { desc = "[g]it [p]review hunk" })
             map("n", "<leader>gs", gs.stage_hunk, { desc = "[g]it [s]tage hunk" })
             map("n", "<leader>gr", gs.reset_hunk, { desc = "[g]it [r]eset hunk" })
-            map('v', '<leader>gs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
-                { desc = "[g]it [s]tage hunk" })
-            map('v', '<leader>gr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
-                { desc = "[g]it [r]eset hunk" })
+            map("v", "<leader>gs", function()
+                gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+            end, { desc = "[g]it [s]tage hunk" })
+            map("v", "<leader>gr", function()
+                gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+            end, { desc = "[g]it [r]eset hunk" })
 
-            map("n", "<leader>gb", function() gs.blame_line { full = true } end, { desc = "[g]it show [b]lame" })
+            map("n", "<leader>gb", function()
+                gs.blame_line({ full = true })
+            end, { desc = "[g]it show [b]lame" })
         end,
     },
 }
