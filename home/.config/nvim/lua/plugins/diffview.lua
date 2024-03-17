58afc6f75b4d9187ce58db0234e7b3a7f3df06c3
Kacper Sagnowski <ksagnowski@gmail.com>
2024-03-17 23:11:41 +0100

    Add separate file and config for diffview

---
+++ home/.config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,12 @@
+return {
+    "sindrets/diffview.nvim",
+    config = function()
+        require("diffview").setup({
+            file_panel = {
+                win_config = {
+                    position = "right",
+                },
+            },
+        })
+    end,
+}
