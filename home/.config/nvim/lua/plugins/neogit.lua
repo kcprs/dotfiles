f170f97a7e55aa7ab5ec2aca4d197fa1d6000b4f
Kacper Sagnowski <ksagnowski@gmail.com>
2024-03-17 18:05:16 +0100

    Switch to neogit for git UI

---
+++ home/.config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,16 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+  config = function()
+    require("neogit").setup {
+      disable_line_numbers = false,
+      graph_style = "unicode",
+    }
+    vim.keymap.set("n", "<leader>G", require("neogit").open, { desc = "open [G]it GUI" })
+  end
+}
