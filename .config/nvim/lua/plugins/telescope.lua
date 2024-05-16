df6c3392124705015e61fbb4bfbc68e64125cbe5
Kacper Sagnowski <ksagnowski@gmail.com>
2024-05-16 22:59:50 +0200

    Include contents of hidden files in telescope searches

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -53,6 +53,16 @@ return {
                 },
 
                 file_ignore_patterns = { "%.git/" },
+                vimgrep_arguments = {
+                    "rg",
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                    "--hidden",
+                }
             },
         })
 
