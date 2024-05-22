e7f6a49e30fc4642b4ae333ab2040860155e2490
Kacper Sagnowski <ksagnowski@gmail.com>
2024-05-22 17:38:19 +0200

    Do not use toggleterm

--- .config/nvim/lua/plugins/toggleterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "akinsho/toggleterm.nvim",
-    config = function()
-        require("toggleterm").setup({
-            open_mapping = require("custom.keymaps").toggleterm_get_open_mapping(),
-            terminal_mappings = true,
-        })
-    end,
-}
