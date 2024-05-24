362f3b0f5a4366543233ba9b6dea1d381dc95cb7
Kacper Sagnowski <ksagnowski@gmail.com>
2024-05-24 21:57:06 +0200

    Set status line to be global

--- .config/nvim/lua/custom/options.lua
+++ .config/nvim/lua/custom/options.lua
@@ -1,6 +1,7 @@
 -- Set highlight on search
 vim.o.hlsearch = false
 
+-- Line numbers
 vim.o.number = true
 vim.o.relativenumber = true
 
@@ -17,7 +18,7 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
--- Keep signcolumn on by default
+-- Keep one signcolumn on by default
 vim.wo.signcolumn = "yes"
 
 -- Decrease update time
@@ -27,10 +28,11 @@ vim.o.timeoutlen = 300
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+-- Cursor stuff
 vim.o.cursorline = true
-
 vim.o.scrolloff = 8
 
+-- Window splitting
 vim.o.splitright = true
 vim.o.splitbelow = true
 
@@ -40,3 +42,8 @@ vim.o.exrc = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 0
 vim.o.softtabstop = 0
+
+-- Configure status line to be global
+vim.o.laststatus = 3
+vim.api.nvim_set_hl(0, 'WinSeparator', { bg = 'NONE' })
+
