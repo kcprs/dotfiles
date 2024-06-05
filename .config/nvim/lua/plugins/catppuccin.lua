627fd8b81931b19432b8c694668cf53dc9de7427
Kacper Sagnowski <ksagnowski@gmail.com>
2024-06-05 22:11:21 +0200

    Modify line number colors in catpuccin

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -3,6 +3,16 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+        require("catppuccin").setup({
+            -- Override the color of relative line numbers - they are too dark by default
+            custom_highlights = function(colors)
+                return {
+                    -- LineNr = { fg = colors.surface1 } -- Original
+                    -- LineNr = { fg = colors.surface2 } -- Slightly brighter
+                    LineNr = { fg = colors.lavender } -- Same as line with cursor
+                }
+            end
+        })
         vim.cmd.colorscheme("catppuccin-mocha")
     end,
 }
