b36229ca0cc6c2b3201514ba11e8eaf5667cffa6
Kacper Sagnowski <ksagnowski@gmail.com>
2024-06-30 20:21:38 +0200

    Prevent confusing window titles in WezTerm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,4 +8,9 @@ config.cursor_blink_rate = 0
 -- Tabs
 config.hide_tab_bar_if_only_one_tab = true
 
+-- Prevent confusing window titles
+wezterm.on('format-window-title', function()
+  return "WezTerm"
+end)
+
 return config
