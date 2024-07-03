92d483a75534a3d1ab3322d27fb149636f04c35e
Kacper Sagnowski <ksagnowski@gmail.com>
2024-07-03 14:39:02 +0200

    Refine Diffview config

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -3,12 +3,20 @@ return {
     config = function()
         require("diffview").setup({
             enhanced_diff_hl = true, -- See |diffview-config-enhanced_diff_hl|
-            file_panel = {
-                win_config = {
-                    position = "right",
-                },
-            },
+            -- file_panel = {
+            --     win_config = {
+            --         position = "right",
+            --     },
+            -- },
             hooks = {
+                view_opened = function()
+                    -- Set move cursor to current file as opposed to tree view
+                    print("DiffviewOpen autocmd triggered")
+                    vim.cmd([[
+                        wincmd l
+                        wincmd l
+                    ]])
+                end,
                 diff_buf_win_enter = function()
                     vim.opt_local.foldmethod = "manual"
                     vim.opt_local.foldlevel = 99
