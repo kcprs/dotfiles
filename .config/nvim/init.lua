72eba717baf942aeffcc248613bf3ecb6712de08
Kacper Sagnowski <ksagnowski@gmail.com>
2024-07-04 14:50:21 +0200

    Disable notification on config chane in lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,7 +17,14 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup({
+    spec = {
+        { import = "plugins" },
+    },
+    change_detection = {
+        notify = false,
+    },
+})
 
 keymaps.setup_basic()
 
