df5ebb72026dbb7ff79d7f2fa151fa9e27b1ce6e
Kacper Sagnowski <ksagnowski@gmail.com>
2024-07-17 15:12:27 +0200

    Add custom conditions for enabling LSPs

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -24,6 +24,15 @@ local function clangd_on_attach(client, bufnr)
     require("custom.keymaps").lsp_clangd(bufnr)
 end
 
+local function use_deno()
+    local env_value = os.getenv("USE_DENO_LSP")
+    if env_value then
+        return true
+    else
+        return false
+    end
+end
+
 local servers = {
     clangd = {
         args = {
@@ -42,9 +51,17 @@ local servers = {
         }
     },
     pyright = {},
-    rust_analyzer = {}, -- Note - configured using rustaceanvim
+    rust_analyzer = {
+        -- Never explicitly call setup() - done by rustaceanvim
+        condition = function() return false end,
+    },
     taplo = {},
-    tsserver = {},
+    tsserver = {
+        condition = function() return not use_deno() end,
+    },
+    denols = {
+        condition = use_deno
+    },
     yamlls = {},
     bashls = {},
 
@@ -106,9 +123,16 @@ return {
 
         require("mason-lspconfig").setup_handlers({
             function(server_name)
-                local default_config = require('lspconfig')[server_name].document_config.default_config
                 local server_config = (servers[server_name] or {})
 
+                -- If condition was given an it evaluates to false, do not set up anything
+                if server_config.condition and not server_config.condition() then
+                    return
+                end
+
+                local default_config = require('lspconfig')[server_name].document_config.default_config
+
+                -- Add args to cmd
                 local cmd = default_config.cmd
                 vim.list_extend(cmd, server_config.args or {})
 
@@ -119,10 +143,6 @@ return {
                     settings = server_config.settings,
                     filetypes = server_config.filetypes,
                 })
-            end,
-
-            ["rust_analyzer"] = function()
-                -- Do nothing - rustaceanvim sets itself up
             end,
         })
 
