94e883ee5452e8dbb91c5361142184d9ed7bb045
Kacper Sagnowski <ksagnowski@gmail.com>
2024-09-13 11:21:44 +0200

    Remove overcomplicated DAP <-> mise confgi sharing

--- .config/nvim/lua/custom/dap.lua
+++ .config/nvim/lua/custom/dap.lua
@@ -1,21 +1,5 @@
 local M = {}
 
-function M.get_task_args(task)
-    local result = vim.system({ ".mise/tasks/" .. task, "-a" }, { text = true }):wait()
-
-    if result.code ~= 0 then
-        vim.notify("Failed to retrieve args for task " .. task .. "\n" .. vim.inspect(result), vim.log.levels.WARN)
-        return {}
-    end
-
-    local lines = vim.split(result.stdout, "\n")
-    lines = vim.tbl_filter(function(line)
-        return line ~= ""
-    end, lines)
-
-    return lines
-end
-
 function M.set_conditional_breakpoint()
     local condition = vim.fn.input("Enter condition for breakpoint: ")
     require("dap").set_breakpoint(condition)
