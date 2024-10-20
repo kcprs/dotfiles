b4340116ddaa57f3792afeb0b6f312264f390770
Kacper Sagnowski <ksagnowski@gmail.com>
2024-10-20 17:26:06 +0200

    Add tailwind-tools.nvim and lspkind.nvim

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -6,12 +6,15 @@ return {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
+        "onsails/lspkind.nvim",
+        "luckasRanarison/tailwind-tools.nvim",
     },
     config = function()
         vim.opt.completeopt = { "menu", "menuone", "noselect" }
         -- vim.opt.shortmess:append("c") -- TODO: try out
 
         local cmp = require("cmp")
+        local lspkind = require("lspkind")
         local ls = require("luasnip")
         cmp.setup({
             sources = {
@@ -25,6 +28,21 @@ return {
                 expand = function(args)
                     ls.lsp_expand(args.body)
                 end
+            },
+            ---@diagnostic disable-next-line: missing-fields
+            formatting = {
+                format = lspkind.cmp_format({
+                    mode = "symbol", -- show only symbol annotations
+                    maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                    -- can also be a function to dynamically calculate max width such as
+                    -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+                    ellipsis_char = "...",    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                    show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+                    -- The function below will be called before any actual modifications from lspkind
+                    -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                    before = require("tailwind-tools.cmp").lspkind_format
+                })
             }
         })
 
