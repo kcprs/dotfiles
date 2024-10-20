4fec36a1e0e765341f1c45201e33e2d23d63a2ae
Kacper Sagnowski <ksagnowski@gmail.com>
2024-10-20 17:21:59 +0200

    Clean up treesitter config, fix indentation in C and Cpp

--- .config/nvim/lua/custom/autocommands.lua
+++ .config/nvim/lua/custom/autocommands.lua
@@ -1,6 +1,15 @@
-local augroup = vim.api.nvim_create_augroup("FileTypeDetection", { clear = true })
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-  group = augroup,
-  pattern = ".envrc",
-  command = "setfiletype sh"
+local augroup_ft = vim.api.nvim_create_augroup("FileTypeDetection", { clear = true })
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    group = augroup_ft,
+    pattern = ".envrc",
+    command = "setfiletype sh"
+})
+
+local augroup_cindent = vim.api.nvim_create_augroup("CIndentation", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    group = augroup_cindent,
+    pattern = "c",
+    callback = function()
+        vim.bo.cindent = true
+    end,
 })
