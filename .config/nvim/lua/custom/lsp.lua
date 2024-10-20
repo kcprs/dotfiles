f96035fe9b8fbcf954385409ca4c6d0b48412742
Kacper Sagnowski <ksagnowski@gmail.com>
2024-10-20 17:14:00 +0200

    Fix disabling virtual text from LSP

--- .config/nvim/lua/custom/lsp.lua
+++ .config/nvim/lua/custom/lsp.lua
@@ -1,16 +1,16 @@
 local M = {}
 
-local show_virtual_text = true
-
-function M.toggle_diagnostics_virtual_text()
-    show_virtual_text = not show_virtual_text
-    vim.diagnostic.show(nil, nil, nil, {
-      virtual_text = show_virtual_text
-    })
+function M.is_virtual_text_enabled()
+    local global_config = vim.diagnostic.config()
+    -- nil is never returned if first arg to config() is not given.
+    ---@diagnostic disable-next-line: need-check-nil
+    return global_config.virtual_text
 end
 
-function M.is_virtual_text_enabled()
-    return show_virtual_text
+function M.toggle_diagnostics_virtual_text()
+    vim.diagnostic.config({
+        virtual_text = not M.is_virtual_text_enabled()
+    })
 end
 
 return M
