84af7242f35850a2b6fea4186d712558b4884484
Kacper Sagnowski <ksagnowski@gmail.com>
2024-10-21 13:39:26 +0200

    Disable icons in which-key

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -5,8 +5,5 @@ return {
     -- Detect tabstop and shiftwidth automatically
     "tpope/vim-sleuth",
 
-    -- Useful plugin to show you pending keybinds.
-    { "folke/which-key.nvim", opts = {} },
-
     "mbbill/undotree",
 }
