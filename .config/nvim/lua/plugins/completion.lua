852fa2601a2a155257643043934890f551b76ce2
Kacper Sagnowski <ksagnowski@gmail.com>
2024-12-13 14:51:13 +0100

    Minor plugin config changes

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,7 +1,7 @@
 return {
     "hrsh7th/nvim-cmp",
     dependencies = {
-        "L3MON4D3/LuaSnip",
+        { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
         "saadparwaiz1/cmp_luasnip",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -7,6 +7,9 @@ return
     opts = {
         heading = {
             icons = { "󰎤  ", "󰎧  ", "󰎪  ", "󰎭  ", "󰎱  ", "󰎳  " },
-        }
+        },
+        latex = {
+            enabled = false,
+        },
     },
 }
