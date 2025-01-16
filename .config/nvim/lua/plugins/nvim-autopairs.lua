6636f5291aceb4eaf94baa1d8c4fe0aa4a49dac8
Kacper Sagnowski <ksagnowski@gmail.com>
2025-01-16 12:08:39 +0100

    Remove remains of nvim-cmp from the config

--- .config/nvim/lua/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/plugins/nvim-autopairs.lua
@@ -2,9 +2,5 @@ return {
     "windwp/nvim-autopairs",
     config = function()
         require("nvim-autopairs").setup()
-        require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
     end,
-    dependencies = {
-        "hrsh7th/nvim-cmp",
-    },
 }
