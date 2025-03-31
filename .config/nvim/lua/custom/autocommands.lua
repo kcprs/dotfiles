790321c37bbe54c97b2564f7a9366a9db77d4c55
Kacper Sagnowski <ksagnowski@gmail.com>
2025-03-31 18:51:17 +0200

    Add line numbers to terminal windows

--- .config/nvim/lua/custom/autocommands.lua
+++ .config/nvim/lua/custom/autocommands.lua
@@ -29,3 +29,12 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.bo.cindent = true
     end,
 })
+
+local augroup_terminal = vim.api.nvim_create_augroup("Terminal", { clear = true })
+vim.api.nvim_create_autocmd("TermOpen", {
+    group = augroup_terminal,
+    callback = function()
+        vim.wo.number = true
+        vim.wo.relativenumber = true
+    end,
+})
