fc2a4bbc11731a6430907af5b87cf03125df903c
Kacper Sagnowski <ksagnowski@gmail.com>
2025-09-08 09:53:08 +0200

    Update conform.nvim config

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,10 +3,13 @@ return {
     config = function()
         require("conform").setup({
             formatters_by_ft = {
-                lua = { "stylua" },
-                python = { "isort", "black" },
-                rust = { "rustfmt", lsp_format = "fallback" },
+                c = { "clang-format", lsp_format = "fallback" },
+                cpp = { "clang-format", lsp_format = "fallback" },
+                cmake = { "cmake-format" },
                 javascript = { "prettier" },
+                lua = { "stylua" },
+                python = { "ruff_organize_imports", "ruff_format" },
+                rust = { "rustfmt", lsp_format = "fallback" },
                 sh = { "shfmt" },
                 -- Use the "_" filetype to run formatters on filetypes that don't
                 -- have other formatters configured.
