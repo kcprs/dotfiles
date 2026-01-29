2f21182d95a8ed6a7aa81f9151ed0b6d19b87c77
Kacper Sagnowski <ksagnowski@gmail.com>
2026-01-29 11:41:05 +0100

    fzf-lua: Change "toggle-help" mapping to a working key combination

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -5,10 +5,10 @@ return {
         require("fzf-lua").setup({
             fzf_opts = { ["--cycle"] = true },
             keymap = {
-                -- builtin = {
-                --     true,
-                --     ["<c-/>"] = "toggle-help", -- stopped working after update...
-                -- },
+                builtin = {
+                    true,
+                    ["<c-j>"] = "toggle-help",
+                },
                 fzf = {
                     true,
                     ["ctrl-q"] = "select-all+accept",
