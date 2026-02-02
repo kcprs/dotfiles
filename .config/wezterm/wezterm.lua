d76b4904e3822de33b0daf7fd363735e4005ed32
Kacper Sagnowski <ksagnowski@gmail.com>
2026-02-02 11:34:23 +0100

    WezTerm: Make Shift+Enter work in zellij

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,4 +13,13 @@ wezterm.on('format-window-title', function()
   return "WezTerm"
 end)
 
+config.keys = {
+  -- Make Shift+Enter work in zellij
+  {
+    key = 'Enter',
+    mods = 'SHIFT',
+    action = wezterm.action.SendString("\x1b[200~\n\x1b[201~"),
+  },
+}
+
 return config
