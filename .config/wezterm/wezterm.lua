354f98c10484a03c96e39059af45ae12f30e8042
Kacper Sagnowski <ksagnowski@gmail.com>
2026-03-19 14:07:19 +0100

    Clean up WezTerm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,25 +1,22 @@
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
 
--- Visuals
-config.color_scheme = "Catppuccin Mocha"
-config.cursor_blink_rate = 0
+return {
+  -- Visuals
+  color_scheme = "Catppuccin Mocha",
+  cursor_blink_rate = 0,
 
--- Tabs
-config.hide_tab_bar_if_only_one_tab = true
+  -- Tabs
+  hide_tab_bar_if_only_one_tab = true,
 
--- Prevent confusing window titles
-wezterm.on('format-window-title', function()
-  return "WezTerm"
-end)
+  -- No title bar needed
+  window_decorations = "RESIZE",
 
-config.keys = {
-  -- Make Shift+Enter work in zellij
-  {
-    key = 'Enter',
-    mods = 'SHIFT',
-    action = wezterm.action.SendString("\x1b[200~\n\x1b[201~"),
-  },
+  keys = {
+    -- Make Shift+Enter work in zellij
+    {
+      key = 'Enter',
+      mods = 'SHIFT',
+      action = wezterm.action.SendString("\x1b[200~\n\x1b[201~"),
+    },
+  }
 }
-
-return config
